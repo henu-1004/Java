e8dacb957933d483912824a19682d015863a7f46
sho5223 <sho5223@naver.com>
2025-07-11 12:20:30 +0900

    git push first_25.07.11

---
+++ CodingTest/src/step01/Main12.java
@@ -0,0 +1,24 @@
+package step01;
+
+import java.util.Scanner;
+
+/* 날짜 : 2025.07.10
+ * 이름 : 서현우
+ * 
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ 
+ */
+public class Main12 {
+
+	public static void main(String[] args) {
+		System.out.print(
+				"\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+	}
+
+}
